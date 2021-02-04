792604073fc5f92f65d52d77f981607fa2d1d89c
BEB <boufcoulman@hotmail.fr>
2021-02-04 22:50:00 +0100

    :tada: retrait limitation de channel discord

--- wholesome_translator_bot.py
+++ wholesome_translator_bot.py
@@ -12,7 +12,6 @@ from functools import reduce
 # DISCORD_GUILD='{nom_du_serveur}'
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 # Permet de definir le nombre de messages à garder en scrutation pour des suppressions notamment
@@ -24,11 +23,9 @@ async def on_ready():
     '''
     Indique que le bot s'est correctement connecté
     '''
-    guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
+        f'{client.user} is connected\n'
     )
 
 
@@ -37,10 +34,6 @@ async def on_reaction_add(reaction, user):
     '''
     Se déclenche dès qu'un utilisateur ajoute une réaction
     '''
-    # Ne se déclenche que pour les messages du serveur voulu
-    if str(reaction.message.guild) != GUILD:
-        return
-
     # Empêche le déclenchement du bot par lui même
     if user == client.user:
         return
