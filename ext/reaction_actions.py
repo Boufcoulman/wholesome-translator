107e7b0de1dfbe28e58fcac99e2dc2701f9ccd21
BEB <boufcoulman@hotmail.fr>
2021-04-09 00:49:22 +0200

    :art: separation of reaction test functions from reaction handling class

--- ext/reaction_actions.py
+++ ext/reaction_actions.py
@@ -1,5 +1,6 @@
 import lib.bing as bing
 from discord.ext import commands
+import asyncio
 
 
 bingtranslate = bing.BingTranslate()
@@ -14,30 +15,45 @@ class ReactionsCog(commands.Cog, name="Bot reactions actions"):
         """Triggered every time a reaction is added.
 
         Args:
-            payload : RawReactionActionEvent containing reaction infos
+            payload: RawReactionActionEvent containing reaction infos
         """
         # Keep the bot from triggering himself
         if payload.user_id == self.bot.user.id:
             return
 
-        # If reaction is the red flag ':triangular_flag_on_post:'
-        if ascii(payload.emoji.name) == r"'\U0001f6a9'":
+        # Handle the calls of on_raw_reaction_add actions
+        await asyncio.wait([
+            translate_on_flag(payload, self.bot),
+        ])
 
-            # Message source
-            channel = self.bot.get_channel(payload.channel_id)
-            message = await channel.fetch_message(payload.message_id)
-            src_msg = message.content
 
-            # Getting translation infos
-            translation, src_lang = bingtranslate.translate(src_msg, 'fr')
+async def translate_on_flag(payload, bot):
+    """Translate the reacted message if the emoji is the red flag
 
-            # Send traduction to private message of the user reacting
-            user = await self.bot.fetch_user(payload.user_id)
-            await user.create_dm()
-            await user.dm_channel.send(
-                f"'{src_msg}'\ntraduit du {src_lang} en\n'{translation}'",
-            )
+    Args:
+        payload: RawReactionActionEvent containing reaction infos
+        bot: the bot
+    """
+    # If reaction is the red flag ':triangular_flag_on_post:'
+    if ascii(payload.emoji.name) == r"'\U0001f6a9'":
+
+        # Message source
+        channel = bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        src_msg = message.content
+
+        # Getting translation infos
+        translation, src_lang = bingtranslate.translate(src_msg, 'fr')
+
+        # Send traduction to private message of the user reacting
+        user = await bot.fetch_user(payload.user_id)
+        await user.create_dm()
+        await user.dm_channel.send(
+            f"'{src_msg}'\ntraduit du {src_lang} en\n'{translation}'",
+        )
 
 
 def setup(bot):
+    """Function run by the bot.load_extension() call from main file
+    """
     bot.add_cog(ReactionsCog(bot))
