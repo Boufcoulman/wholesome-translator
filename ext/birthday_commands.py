7aa887b8600a61472d54e4722b39d2f993d64c7e
BEB <boufcoulman@hotmail.fr>
2021-07-22 05:47:16 +0200

    :tada: Parse user !

--- ext/birthday_commands.py
+++ ext/birthday_commands.py
@@ -1,9 +1,11 @@
 from discord.ext import commands
 import re
 import lib.birthday_lib as bd_lib
+import discord
 
 
-BIRTHDAY_RE = re.compile(r'\d{4}')
+USER_TAG_RE = re.compile(r'.*#\d{4}')
+USER_ID_RE = re.compile(r'\d{18}')
 
 
 class BirthdayCmdCog(commands.Cog, name="Translate bot commands"):
@@ -25,37 +27,60 @@ class BirthdayCmdCog(commands.Cog, name="Translate bot commands"):
         Args:
             user: the user tag of the birthday person
             date: the birthday date "dd-mm"
+
+        Usage : %update Mudae#0807 27-05
         """
 
         await ctx.send(f"Oui voilà user:{user} et date:{date}.")
-
-        # Unified birth date formalism
-        birthday = str.replace('-', '')
-
-        # Checker https://dateutil.readthedocs.io/en/stable/parser.html
-        # pipenv install python-dateutil
-        # from dateutil.parser import parse
-        # parse("11 05",dayfirst=True, yearfirst=False)
-        # >>> datetime.datetime(2021, 5, 11, 0, 0)
-
-        # Checker ??? https://returns.readthedocs.io/en/latest/#id1
 
         if BIRTHDAY_RE.match(birthday):
             birthday = date
             await ctx.send("Format mmdd")
         elif ALT_BIRTHDAY_RE.match(date):
-            birthday =
+            # birthday =
             await ctx.send("Format mm-dd")
         else:
             await ctx.send("ça ne fonctionne pas :/")
 
-    @commands.command(name="birthday.list", aliases=["bd.list"])
+    async def parse_user(self, ctx, user) -> discord.User.id:
+        """Verify that user exists and returns it's discord id if so
+
+        Args:
+            user: the user to try to find (id or name#discriminator)
+
+        Returns:
+            user_id: discord id of the user if found, None otherwise
+        """
+        id_match = USER_ID_RE.search(user)
+        tag_match = USER_TAG_RE.search(user)
+
+        # Check if input user exists
+        if id_match:
+            # Try to found user with parsed id
+            found_user = discord.utils.get(
+                ctx.guild.members,
+                id=int(id_match.group(0))
+            )
+        elif tag_match:
+            name, discriminator = tag_match.group(0).split('#')
+            # Try to found user with parsed name and discriminator
+            found_user = discord.utils.get(
+                ctx.guild.members,
+                name=name,
+                discriminator=discriminator
+            )
+        else:
+            found_user = None
+
+        return found_user.id if found_user else None
+
+    @ commands.command(name="birthday.list", aliases=["bd.list"])
     async def list(self, ctx) -> None:
         """Certes
         """
         await ctx.send("Oui voilà la liste.")
 
-    @commands.command(name="birthday.delete", aliases=["bd.delete"])
+    @ commands.command(name="birthday.delete", aliases=["bd.delete"])
     async def delete(self, ctx, user) -> None:
         """Certes
         """
