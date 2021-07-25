f8182f14b3d75bbcd837ca0d067f901e0ac9c3f6
BEB <boufcoulman@hotmail.fr>
2021-07-25 22:23:57 +0200

    :art: Improve main script logic + init birthday db

--- wholesome_translator_bot.py
+++ wholesome_translator_bot.py
@@ -7,6 +7,8 @@ from discord.ext import commands
 from lib.load_var import get_var
 import discord
 
+from lib.birthday_lib import init_birthday_db
+
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.StreamHandler())
@@ -14,6 +16,8 @@ log.addHandler(logging.StreamHandler())
 TOKEN = get_var('DISCORD_TOKEN')
 CMD_PREFIX = get_var('CMD_PREFIX', '%')
 
+log.info('Initialisation de la base de données anniversaire...')
+init_birthday_db()
 
 log.debug('Creating bot...')
 # Add intent in order to gather member datas
@@ -30,15 +34,6 @@ initial_extensions = [
     'ext.reaction_actions',
 ]
 
-# Load of commands extentions
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception:
-            print(f'Failed to load extension {extension}.', file=sys.stderr)
-            traceback.print_exc()
-
 
 @bot.event
 async def on_ready():
@@ -47,5 +42,13 @@ async def on_ready():
 
 
 if __name__ == '__main__':
+    # Load of commands extentions
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
     log.info('Starting bot...')
     bot.run(TOKEN, reconnect=True)
